5a83dbdc92a8411ebe21d76e28c8e89f95540ba1
Thuy Nguyen <ducthuy@gmail.com>
2025-08-14 09:26:49 +0700

    Add search and replace

--- src/js/tinymce.mjs
+++ src/js/tinymce.mjs
@@ -1,53 +1,65 @@
 export default function initTinymce() {
-    tinymce.init({
-        selector: '#wysiwyg',
-        license_key: 'gpl',
-        browser_spellcheck: false,
-        promotion: false,
-        //skin: 'oxide-dark',
-        //content_css: 'dark',
-        resize: false,
-        branding: false,
-        width: '100%',
-        height: '100%',
-        plugins: ['link', 'lists', 'table'],
-        toolbar: [
-            { name: 'history', items: ['undo', 'redo'] },
-            { name: 'styles', items: ['styles'] },
-            { name: 'formatting', items: ['bold', 'italic', 'underline'] },
-            { name: 'alignment', items: ['alignleft', 'aligncenter', 'alignright', 'alignjustify'] },
-            { name: 'link', items: ['link'] },
-            { name: 'color', items: ['forecolor', 'backcolor'] },
-            { name: 'lists', items: ['bullist', 'numlist'] },
-            { name: 'indentation', items: ['outdent', 'indent'] },
-            { name: 'table', items: ['table'] }
-        ],
-        setup: tinymceSetup
-    });
+	tinymce.init({
+		selector: "#wysiwyg",
+		license_key: "gpl",
+		browser_spellcheck: false,
+		promotion: false,
+		//skin: 'oxide-dark',
+		//content_css: 'dark',
+		resize: false,
+		branding: false,
+		width: "100%",
+		height: "100%",
+		plugins: ["link", "lists", "table", "searchreplace"],
+		toolbar: [
+			{ name: "history", items: ["undo", "redo"] },
+			{ name: "styles", items: ["styles"] },
+			{ name: "formatting", items: ["bold", "italic", "underline"] },
+			{
+				name: "alignment",
+				items: [
+					"alignleft",
+					"aligncenter",
+					"alignright",
+					"alignjustify",
+				],
+			},
+			{ name: "link", items: ["link"] },
+			{ name: "color", items: ["forecolor", "backcolor"] },
+			{ name: "lists", items: ["bullist", "numlist"] },
+			{ name: "indentation", items: ["outdent", "indent"] },
+			{ name: "table", items: ["table"] },
+			{ name: "searchreplace", items: ["searchreplace"] },
+		],
+		setup: tinymceSetup,
+	});
 }
 
 function tinymceSetup(editor) {
-    function triggerEvent() {
-        const updateEvent = new CustomEvent("wysiwygChanged", {
-            detail: {
-                timestamp: Date.now(),
-                data: editor.getContent({ format: 'html' }),
-            },
-        });
-        document.dispatchEvent(updateEvent);
-    }
+	function triggerEvent() {
+		const updateEvent = new CustomEvent("wysiwygChanged", {
+			detail: {
+				timestamp: Date.now(),
+				data: editor.getContent({ format: "html" }),
+			},
+		});
+		document.dispatchEvent(updateEvent);
+	}
 
-    document.querySelector('[name="spell"]').addEventListener('change', evt => {
-        const body = editor.getBody();
-        body.setAttribute('spellcheck', evt.currentTarget.checked);
-    });
+	document
+		.querySelector('[name="spell"]')
+		.addEventListener("change", (evt) => {
+			const body = editor.getBody();
+			body.setAttribute("spellcheck", evt.currentTarget.checked);
+		});
 
-    document.addEventListener('codeMirrorChanged', async evt => {
-        if (editor.hasFocus()) {
-            return;
-        }
-        editor.setContent(evt.detail.data);
-    });
-    editor.on('change', triggerEvent);
-    editor.on('input', triggerEvent);
+	document.addEventListener("codeMirrorChanged", async (evt) => {
+		if (editor.hasFocus()) {
+			return;
+		}
+		editor.setContent(evt.detail.data);
+	});
+	editor.on("change", triggerEvent);
+	editor.on("input", triggerEvent);
 }
+
